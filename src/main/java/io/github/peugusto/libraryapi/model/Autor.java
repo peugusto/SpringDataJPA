0635d767c52a5a5736c9c4a6674e8a167ccfe2ff
peugusto <pedrooknd@gmail.com>
2026-04-23 14:39:02 -0300

    feat: configurei o datasource e fiz o mapeamento

---
+++ src/main/java/io/github/peugusto/libraryapi/model/Autor.java
@@ -0,0 +1,30 @@
+package io.github.peugusto.libraryapi.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name="autor")
+@Getter
+@Setter
+public class Autor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "nome", length = 100, nullable = false)
+    private String nome;
+
+    @Column(name = "data_nascimento", nullable = false)
+    private LocalDate dataNascimento;
+
+    @Column(name = "nacionalidade" , length = 50, nullable = false)
+    private String nacionalidade;
+
+
+}
