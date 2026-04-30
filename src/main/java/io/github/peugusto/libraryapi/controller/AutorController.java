f69b9ab42c50ccceb33fe20668cf22291d3c2f51
peugusto <pedrooknd@gmail.com>
2026-04-30 00:59:52 -0300

    feat: findall, e update feito

--- src/main/java/io/github/peugusto/libraryapi/controller/AutorController.java
+++ src/main/java/io/github/peugusto/libraryapi/controller/AutorController.java
@@ -8,8 +8,10 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/autores")
@@ -60,5 +62,42 @@ public class AutorController {
         }
 
         return ResponseEntity.notFound().build(); // Retorna 404
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AutorDTO>> pesquisar(
+            @RequestParam(value = "nome", required = false) String nome,
+            @RequestParam(value = "nacionalidade", required = false) String nacionalidade){
+
+        List<Autor> resultado = service.pesquisa(nome,nacionalidade);
+        List<AutorDTO> lista = resultado
+                .stream()
+                .map(
+                autor -> new AutorDTO(
+                        autor.getId(),
+                        autor.getNome(),
+                        autor.getDataNascimento(),
+                        autor.getNacionalidade())
+        ).collect(Collectors.toList());
+
+        return ResponseEntity.ok(lista);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Void> atualizar(@PathVariable("id") String id, @RequestBody AutorDTO dto){
+        var idAutor = UUID.fromString(id);
+
+        Optional<Autor> autor = service.obterPorId(idAutor);
+
+        autor.ifPresentOrElse(
+                obj -> {
+                    obj.setNome(dto.nome());
+                    obj.setNacionalidade(dto.nascionalidade());
+                    obj.setDataNascimento(dto.dataNascimento());
+                }, () -> ResponseEntity.notFound().build()
+        );
+
+        service.atualizar(autor.get());
+        return  ResponseEntity.noContent().build();
     }
 }
--- src/main/java/io/github/peugusto/libraryapi/model/Autor.java
+++ src/main/java/io/github/peugusto/libraryapi/model/Autor.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
@@ -39,7 +40,7 @@ public class Autor {
     @Column(name = "data_cadastro")
     private LocalDateTime dataCadastro;
 
-    @LastModifiedBy
+    @LastModifiedDate
     @Column(name = "data_atualizacao")
     private LocalDateTime dataAtualizacao;
 }
